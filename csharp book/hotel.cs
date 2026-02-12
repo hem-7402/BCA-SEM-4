9dc1c1c55e6fda2f3fbe41295dd93d882c6878ec
HEMAL GOSAI <gosaihemal72@gmail.com>
2026-02-12 09:25:25 +0530

    Add files via upload

--- csharp book/hotel.cs
+++ csharp book/hotel.cs
@@ -1,89 +1,101 @@
 using System;
-
-class HotelMenu
-{
-    // Method to print final bill
-    static void FinalBill(int[,] select_item, int[] price, string[] menu)
-    {
-        int total = 0;
+class HotelMenu{
+    static void ShowBill(int[] select_item, int[] price, string[] menu){
+        int total=0;
 
         Console.Clear();
-        Console.WriteLine("***** FINAL BILL *****");
-        Console.WriteLine("----------------------------------");
+        Console.WriteLine("************ CURRENT BILL ************");
+        Console.WriteLine("--------------------------------------");
         Console.WriteLine("MENU\t\tQTY\tPRICE");
-        Console.WriteLine("----------------------------------");
+        Console.WriteLine("--------------------------------------");
 
-        for (int i = 0; i < menu.Length; i++)
-        {
-            if (select_item[i, 1] > 0)
-            {
-                Console.WriteLine("{0}\t{1}\t{2}",
-                    menu[i],
-                    select_item[i, 1],
-                    price[i] * select_item[i, 1]);
-
-                total += price[i] * select_item[i, 1];
+        for (int i = 0; i < menu.Length; i++){
+            if (select_item[i] > 0){
+                int itemTotal = price[i] * select_item[i];
+                Console.WriteLine(menu[i] + "\t" + select_item[i] + "\t" + itemTotal);
+                total += itemTotal;
             }
         }
 
-        Console.WriteLine("----------------------------------");
+        Console.WriteLine("--------------------------------------");
         Console.WriteLine("TOTAL AMOUNT : {0}", total);
-        Console.WriteLine("----------------------------------");
-        Console.WriteLine("********** THANK YOU *************");
-        Console.ReadKey();
+        Console.WriteLine("--------------------------------------");
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadLine();
     }
 
-    static void Main()
-    {
-        string[] menu =
-        {
+    static void FinalBill(int[] select_item, int[] price, string[] menu){
+        ShowBill(select_item, price, menu);
+        Console.WriteLine("\n********** THANK YOU *************");
+    }
+
+    static void Main(){
+        string[] menu = {
             "MANCHURIAN",
             "PAV-BHAJI",
-            "NOODLES     ",
+            "NOODLES\t",
             "MANCHOW SOUP"
         };
 
         int[] price = { 199, 149, 199, 99 };
-        int[,] select_item = new int[menu.Length, 2];
+        int[] select_item = new int[menu.Length];
 
         char check;
 
-        do
-        {
+        do{
             Console.Clear();
-            Console.WriteLine("***** HOTEL MENU *****");
+            Console.WriteLine("-------- HOTEL MENU --------");
             Console.WriteLine("1. MANCHURIAN - 199");
             Console.WriteLine("2. PAV-BHAJI  - 149");
-            Console.WriteLine("3. NOODLES   - 199");
+            Console.WriteLine("3. NOODLES    - 199");
             Console.WriteLine("4. MANCHOW SOUP - 99");
-            Console.WriteLine("5. Exit");
+            Console.WriteLine("5. Update Order");
+            Console.WriteLine("6. Show Bill");
+            Console.WriteLine("7. Exit");
             Console.Write("\nEnter your choice: ");
 
             int choice = int.Parse(Console.ReadLine());
 
-            switch (choice)
-            {
+            switch (choice){
                 case 1:
-                    select_item[0, 1]++;
-                    Console.WriteLine("MANCHURIAN added");
-                    break;
-
                 case 2:
-                    select_item[1, 1]++;
-                    Console.WriteLine("PAV-BHAJI added");
-                    break;
-
                 case 3:
-                    select_item[2, 1]++;
-                    Console.WriteLine("NOODLES added");
-                    break;
-
                 case 4:
-                    select_item[3, 1]++;
-                    Console.WriteLine("MANCHOW SOUP added");
+                    Console.Write("Enter quantity: ");
+                    int qty = int.Parse(Console.ReadLine());
+                    select_item[choice - 1] += qty;
+                    Console.WriteLine("Item added successfully!");
                     break;
 
                 case 5:
+                    Console.WriteLine("\n--- Update Order ---");
+                    for (int i = 0; i < menu.Length; i++)
+                    {
+                        Console.WriteLine((i + 1) + ". " + menu[i] +
+                            " (Current Qty: " + select_item[i] + ")");
+                    }
+
+                    Console.Write("Select item to update: ");
+                    int updateChoice = int.Parse(Console.ReadLine());
+
+                    if (updateChoice >= 1 && updateChoice <= menu.Length)
+                    {
+                        Console.Write("Enter new quantity (0 to remove): ");
+                        int newQty = int.Parse(Console.ReadLine());
+                        select_item[updateChoice - 1] = newQty;
+                        Console.WriteLine("Order Updated!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid choice!");
+                    }
+                    break;
+
+                case 6:
+                    ShowBill(select_item, price, menu);
+                    break;
+
+                case 7:
                     FinalBill(select_item, price, menu);
                     return;
 
@@ -95,8 +107,7 @@ class HotelMenu
             Console.Write("\nDo you want to continue (y/n): ");
             check = char.Parse(Console.ReadLine());
 
-            if (check == 'n' || check == 'N')
-            {
+            if (check == 'n' || check == 'N'){
                 FinalBill(select_item, price, menu);
                 return;
             }
